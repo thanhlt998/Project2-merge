1965c82c78c233d9465b866c119c0ae78f58d52d
doannga <doannga1234@gmail.com>
2019-05-06 22:31:33 +0700

    Change file mapping_xpath

--- wrapper/xpath_mapping.py
+++ wrapper/xpath_mapping.py
@@ -40,70 +40,64 @@ class XpathMapping:
         print(result1)
         result = self.combine_xpath(result1)
         return result
-
     @staticmethod
-    def is_different_position(array, string_need_compare, position, attr):
-        for i in array:
-            if string_need_compare != i[attr][position]:
-                return True
-        return False
-
-    @staticmethod
-    def get_same_string(strings):
-        result = ''
-        if len(strings) > 0:
-            last_position = -1
-            first_string = strings[0]
-            for s in strings:
-                if len(s) < len(first_string):
-                    first_string = s
-
-            for i in range(len(first_string)):
-                for s in strings:
-                    if s[i] != first_string[i]:
-                        last_position = i
-                        break
-            if last_position > -1:
-                result = first_string[:last_position]
-                result = result.replace('[', '')
-        return result
-
-    def combine_xpath(self, list_xpath):
-        result = {}
-        labels = ['0.0', '1.0', '2.0', '3.0', '4.0', '5.0', '6.0', '7.0', '8.0', '9.0', '10.0', '11.0', '12.0', '13.0',
-                  '14.0', '15.0', '16.0', '17.0', '18.0', '19.0']
+    def combineXpath(list_xpath):
+        result = []
+        labels = ['0.0', '1.0', '2.0', '3.0', '4.0', '5.0', '6.0', '7.0', '8.0', '9.0', '10.0', '11.0', '12.0', '13.0','14.0', '15.0', '16.0', '17.0', '18.0', '19.0']
         for label in labels:
-            xpaths = []
-            for item in range(len(list_xpath)):
-                if list_xpath[item][0] == label:
-                    index = next((j for j, it in enumerate(xpaths) if it['xpath'] == list_xpath[item][1]), -1)
-                    if index == -1:
-                        xpath_items = list_xpath[item][1].split('/')
-                        xpath_items = [x for x in xpath_items if x.strip()]
-                        xpaths.append({
-                            'xpath': list_xpath[item][1],
-                            'xpathItems': xpath_items
-                        })
+            xpaths = [list_xpath[i][1] for i in range(len(list_xpath)) if list_xpath[i][0] == label]
             if len(xpaths) > 1:
-                combine_xpath = ''
-                min_length = min([len(i['xpathItems']) for i in xpaths])
-                for i in range(min_length):
-                    string_need_compare = xpaths[0]['xpathItems'][i]
-                    if self.is_different_position(xpaths, string_need_compare, i, 'xpathItems'):
-                        different_position = i
-                        break
-                if different_position != -1:
-                    for i in range(different_position):
-                        combine_xpath = combine_xpath + '/' + xpaths[0]['xpathItems'][i]
-                    last_strings = []
-                    if different_position < min_length:
-                        for xpath in xpaths:
-                            last_strings.append(xpath['xpathItems'][different_position])
-                        same_string = self.get_same_string(last_strings)
-                        if same_string != '':
-                            combine_xpath = combine_xpath + '/' + same_string
-                result[label] = combine_xpath
-            if len(xpaths) == 1:
-                result[label] = xpaths[0]['xpath']
-
+                xpathMerge = ''
+                xpathArray = []
+                for xpath in xpaths:
+                    xpathArray.append([x for x in str(xpath).split('/') if x.strip()])
+                xpathMerge = mergeXpaths(xpathArray)
+                result.append([label, xpathMerge])
+            elif len(xpaths) ==1:
+                result.append([label, xpaths[0]])
+        return result
+    @staticmethod
+    def mergeXpaths(xpathArray):
+        result = ''
+        while len(xpathArray) > 0:
+            tags = [i[0] for i in xpathArray if len(i) > 0]
+            if len(tags) <= 1:
+                break
+            if len(tags) > 1:
+                tagMostAppear = getMostAppear(tags)
+                numberAppear = tags.count(tagMostAppear)
+                if numberAppear > 1:
+                    xpathArray = [i for i in xpathArray if len(i) > 0 and i[0] == tagMostAppear]
+                    for x in xpathArray:
+                        x.remove(tagMostAppear)
+                    result = result + '/' + tagMostAppear
+                else:
+                    for x in xpathArray:
+                        x.pop(0)
+                    stringCompare = compareStrings(tags)
+                    if stringCompare != '':
+                        result = result + '/' + compareStrings(tags)
+        return result
+    @staticmethod
+    def compareStrings(strings):
+        result = ''
+        if len(strings) <= 1:
+            return result
+        minLength = min([len(i) for i in strings])
+        if minLength == 0:
+            return result
+        while len(strings) > 0:
+            characters = [c[0] for c in strings if len(c) > 0]
+            characterMostAppear = getMostAppear(characters)
+            numberAppear = characters.count(characterMostAppear)
+            if numberAppear <= 1:
+                break
+            strings = [i for i in strings if len(i) > 0 and i[0] == characterMostAppear]
+            strings = [i[1:] for i in strings]
+            result = result + characterMostAppear
+        result = result.replace('[','')
+        return result
+    @staticmethod
+    def getMostAppear(strings):
+        result = max(strings, key = strings.count)
         return result
