55a70a45fa15ec61595372219b9cce3f334a6537
doannga <doannga1234@gmail.com>
2019-05-10 23:44:41 +0700

    fix bug

--- main.py
+++ main.py
@@ -12,32 +12,27 @@ from setting import SCRAPY_CONFIG, get_domain, WEB_LIST
 
 @defer.inlineCallbacks
 def crawl():
-    # for web in WEB_LIST:
-    #     start_url = web['start_url']
-    #     job_url_selector = web['job_url']
-    #     next_page_selector = web['next_page']
-    #     domain = get_domain(start_url)
-    yield runner.crawl(SchemaCrawler, name="schema_crawler", start_url=start_url, job_url=job_url_selector,
-                            next_page=next_page_selector, domain=domain)
-    yield runner.crawl(XpathCrawler, name="xpath_crawler", domain=domain)
+    for web in WEB_LIST.values():
+        start_url = web['start_url']
+        job_url_selector = web['job_url']
+        next_page_selector = web['next_page']
+        domain = get_domain(start_url)
+        yield runner.crawl(SchemaCrawler, name="schema_crawler", start_url=start_url, job_url=job_url_selector,
+                                next_page=next_page_selector, domain=domain)
+        yield runner.crawl(XpathCrawler, name="xpath_crawler", domain=domain)
 
-    yield runner.crawl(Crawler, name="crawler", domain=domain)
+        yield runner.crawl(Crawler, name="crawler", domain=domain)
     reactor.stop()
 
 
 if __name__ == '__main__':
-
-    # # setting = get_project_settings()
-    # # for key, config in SCRAPY_CONFIG.items():
-    # #     setting[key] = config
-    # # configure_logging()
-    # # runner = CrawlerRunner(setting)
-    # # crawl()
-    # # reactor.run()
-    start_url = "https://careerbuilder.vn/viec-lam/tat-ca-viec-lam-vi.html"
-    job_url_selector = "//div[contains(@class,'gird_standard')]//dd[contains(@class, 'brief')]/span[@class='jobtitle']/h3[@class='job']/a"
-    next_page_selector = "//div[@class='paginationTwoStatus']/a[@class='right']"
-    domain = get_domain(start_url)
+    # start_url = "https://careerbuilder.vn/viec-lam/tat-ca-viec-lam-vi.html"
+    # job_url_selector = "//div[contains(@class,'gird_standard')]//dd[contains(@class, 'brief')]/span[@class='jobtitle']/h3[@class='job']/a"
+    # next_page_selector = "//div[@class='paginationTwoStatus']/a[@class='right']"
+    # start_url = "https://vieclam24h.vn/tim-kiem-viec-lam-nhanh/?hdn_nganh_nghe_cap1=&hdn_dia_diem=&hdn_tu_khoa=&hdn_hinh_thuc=&hdn_cap_bac="
+    # job_url_selector = "//*[@id='cols-right']/div[1]/div[1]/div/div[1]/div/div/div/div[2]/span[1]/a"
+    # next_page_selector = "//ul[@class='pagination']/li[@class='next']/a"
+    # domain = get_domain(start_url)
     setting = get_project_settings()
 
     for key, config in SCRAPY_CONFIG.items():
